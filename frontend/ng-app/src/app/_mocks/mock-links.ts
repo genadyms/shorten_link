a70e4d6911160b3676d2785dcb3816cdef9f3a8c
genadyms <genadyms@gmail.com>
2017-05-18 15:37:10 +0300

    create mock data

---
+++ frontend/ng-app/src/app/_mocks/mock-links.ts
@@ -0,0 +1,7 @@
+import { Link } from '../_models/link';
+
+export const LINKS: Link[] = [
+    { value: 'val_one', description: 'description about val_one'},
+    { value: 'val_two', description: 'description about val_two'},
+    { value: 'val_three', description: 'description about val_three'},
+]
---
+++ frontend/ng-app/src/app/_mocks/mock-user-links.ts
@@ -0,0 +1,7 @@
+import { UserLink } from '../_models/user-link';
+
+export const USER_LINKS: UserLink[] = [
+    { value: 'val_one', description: 'description about val_one', clicks: 1, tags: ['cars']},
+    { value: 'val_two', description: 'description about val_two', clicks: 2, tags: ['it']},
+    { value: 'val_three', description: 'description about val_three', clicks: 3, tags: ['work']},
+]
