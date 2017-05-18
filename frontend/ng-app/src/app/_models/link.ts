1164456b7418e7688128ac1a135f110b930a8857
genadyms <genadyms@gmail.com>
2017-05-18 15:36:52 +0300

    create model classes

---
+++ frontend/ng-app/src/app/_models/link.ts
@@ -0,0 +1,5 @@
+export class Link {
+    value: string;
+    description: string;
+    tags: string[];
+}
---
+++ frontend/ng-app/src/app/_models/user-link.ts
@@ -0,0 +1,5 @@
+import { Link } from './link';
+
+export class UserLink extends Link {
+    clicks: number;
+}
