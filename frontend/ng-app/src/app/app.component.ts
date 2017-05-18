fef44001e84d88521278736f28feed598a9cde06
genadyms <genadyms@gmail.com>
2017-05-18 15:37:51 +0300

    test data

--- frontend/ng-app/src/app/app.component.ts
+++ frontend/ng-app/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Link } from './_models/link';
+import { LINKS } from './_mocks/mock-links';
+import { UserLink } from './_models/user-link';
+import { USER_LINKS } from './_mocks/mock-user-links';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Shorten links app';
+  links: Link[] = LINKS;
+  userLinks: UserLink[] = USER_LINKS;
 }
